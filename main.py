dbe52fee4b1e42f2a0f93896fe149faf98c7defc
James Twose <jamestwose@Mac-mini.home>
2021-11-08 09:21:14 +0100

    Starting on the main.py script for jmsbook, creating get, post, and delete methods

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from typing import Optional
 
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Response, status, HTTPException
 from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
+
+my_posts = [{"title": "I get my drinks in California",
+             "content": "ooo that's it",
+             "id": 1},
+            {"title": "I hurt myself at night",
+             "content": "I think I am not okay",
+             "id": 2}
+            ]
 
 
 class Post(BaseModel):
@@ -13,6 +22,16 @@ class Post(BaseModel):
     rating: Optional[int] = None
 
 
+def find_post(id):
+    output = [x if x["id"] == id else None for x in my_posts]
+    return output[0]
+
+
+def find_post_index(id):
+    output = [i if x["id"] == id else None for i, x in enumerate(my_posts)]
+    return output[0]
+
+
 @app.get("/")
 def read_root():
     return {"message": "welcome to my API"}
@@ -20,16 +39,28 @@ def read_root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is your post"}
+    return {"data": my_posts}
 
 
-@app.post("/createposts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
-    print(post.dict())
-    return {"data": post}
+    post_dict = post.dict()
+    post_dict["id"] = randrange(0, 10000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
 # title: str, content: str
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/posts/{id}")
+def get_post(id: int):
+    post = find_post(id=id)
+    if post is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {id} does not exist")
+    return {"item_id": id, "post": post}
+
+@app.delete("/posts/{id}")
+def delete_post(id: int):
+    index = find_post_index(id)
+    my_posts.pop(index)
+    return {"message": "post was successfully deleted"}
